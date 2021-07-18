243eb4b5bc6395ce7b7cf47597f5201b7facf45b
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2021-07-18 11:38:01 -0400

    added the functions to site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,47 @@
+"use strict"
 
-let helloWorld = () => {
-    alert("Hello World");
+//returns the id of the HTML element
+let $ =(id)=> {
+    return document.getElementById(id);
+}
+
+let makeBold = () => {
+
+    //this array will hold the numbers generated using a for loop
+    let numbers = [];
+
+    //get the start and end number from the user
+    let startValue = $("startValue").value;
+    let endValue = $("endValue").value;
+
+    //convert the values to numbers
+    parseInt(startValue);
+    parseInt(endValue);
+
+    //populate the array using the starting and ending values
+    for(startValue; startValue <= endValue; startValue++) {
+        numbers.push(startValue);
+    }
+    
+    //using the forEach function iterates over the numbers array
+    //and check for even numbers, and make them bold
+    numbers.forEach((num)=>{
+        
+        let newR = $("results").insertRow();
+        let newC = newR.insertCell();
+        newC.innerHTML = num;
+        if(num % 2 == 0) {
+            newR.className = "bold";
+        }
+    });
+}
+
+window.onload = function() {
+    //when the event of click occurs trigger the makeBold function
+    $("btnSubmit").onclick = function() {
+        //this call prevents the page from reprinting the numbers
+        $("results").innerHTML = "";
+        
+        makeBold();
+   }
 }
